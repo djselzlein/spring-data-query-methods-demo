2dd69c17da51867757297fe3ab514d5ce1b734a1
Djeison Selzlein <djeisonaguir@gmail.com>
2017-11-06 16:22:10 -0200

    initial commit

---
+++ src/main/java/com/selzlein/djeison/springdataquerymethodsdemo/SpringDataQueryMethodsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.selzlein.djeison.springdataquerymethodsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataQueryMethodsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataQueryMethodsDemoApplication.class, args);
+	}
+}
